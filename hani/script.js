d7df120d4dbe0f29b0da3e6679ebc8eb4ba2ee21
Hamza Hanif <hamzahanif2210@gmail.com>
2020-12-17 12:54:54 +0500

    Update script.js

--- hani/script.js
+++ hani/script.js
@@ -304,7 +304,7 @@ function reveal() {
   }
   
   var ifrm = document.createElement("iframe");
-        ifrm.setAttribute("src", "https://www.youtube.com/embed/KDxJlW6cxRk?controls=0&loop=1&autoplay=1");
+        ifrm.setAttribute("src", "https://www.youtube.com/embed/XC0yErSs9TM?controls=0&loop=1&autoplay=1");
         //ifrm.style.width = `${w}px`;
         //ifrm.style.height = `${h}px`;
         ifrm.style.border = 'none';
